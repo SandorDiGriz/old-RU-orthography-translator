2ac5b60c4a534c4920222675d26a13e77374805f
Alexander Potekhin <86772207+SandorDiGriz@users.noreply.github.com>
2021-07-25 01:19:35 +0300

    comments upd

--- translator/excep_dict.py
+++ translator/excep_dict.py
@@ -1,5 +1,5 @@
-'''Function to create a list of exceptions from dictionary txt file'''
 def get_dictionary():
+    '''Function to create a list of exceptions from dictionary txt file'''
     lst_dict = []
     with open (r'dictionary.txt', encoding='utf-8') as file:
         for line in file:
